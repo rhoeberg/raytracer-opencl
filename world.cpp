07af47d8b3bf36a0f9a6e7e8e3c7eaf385fc05ef
rhoeberg <contact@rhoeberg.com>
2021-02-14 11:55:57 +0100

    updated world

--- world.cpp
+++ world.cpp
@@ -109,21 +109,21 @@ World InitializeDefaultWorld2()
 World InitializeDefaultWorld()
 {
     World world;
-    world.bgCol = {1.0f, 1.0f, 0.0f};
+    world.bgCol = {0.08f, 0.11f, 0.42f};
     world.ambient = {0.0f, 0.0f, 0.0f};
     //world.sphereCount = 2;
     world.geometryCount = 0;
     world.sphereCount = 0;
     world.planeCount = 0;
     {
-        world.spheres[0].mat.diffuse = {0.0f, 1.0f, 0.0f};
+        world.spheres[0].mat.diffuse = {0.81f, 0.73f, 0.23f};
         world.spheres[0].mat.specular = {0.5f, 0.5f, 0.5f};
         // world.spheres[0].mat.specular = {0.0f, 0.0f, 0.0f};
         world.spheres[0].mat.shine = 100.0f;
         world.spheres[0].c = {-0.8f, 0.2f, -3.0f};
         world.spheres[0].r = 0.5f;
         world.spheres[0].mat.mirror = true;
-        world.spheres[0].mat.reflection = {0.5f, 0.5f, 0.5f};
+        world.spheres[0].mat.reflection = {0.2f, 0.2f, 0.2f};
         world.geometries[world.geometryCount].id = world.sphereCount;
         world.geometries[world.geometryCount].type = Geo_Sphere;
         world.geometryCount += 1;
@@ -131,7 +131,7 @@ World InitializeDefaultWorld()
     }
     
     {
-        world.spheres[1].mat.diffuse = {1.0f, 0.0f, 0.0f};
+        world.spheres[1].mat.diffuse = {0.8f, 0.78f, 0.8f};
         world.spheres[1].mat.specular = {0.5f, 0.5f, 0.5f};
         // world.spheres[1].mat.specular = {0.0f, 0.0f, 0.0f};
         world.spheres[1].mat.shine = 100.0f;
@@ -139,7 +139,7 @@ World InitializeDefaultWorld()
         world.spheres[1].c = center;
         world.spheres[1].r = 0.5f;
         world.spheres[1].mat.mirror = true;
-        world.spheres[1].mat.reflection = {0.5f, 0.5f, 0.5f};
+        world.spheres[1].mat.reflection = {0.2f, 0.2f, 0.2f};
         world.geometries[world.geometryCount].id = world.sphereCount;
         world.geometries[world.geometryCount].type = Geo_Sphere;
         world.geometryCount += 1;
@@ -152,20 +152,20 @@ World InitializeDefaultWorld()
         world.planes[0].mat.diffuse = {0, 1, 0};
         world.planes[0].mat.specular = {0.2f, 0.2f, 0.2f};
 		world.planes[0].mat.shine = 0.0f;
-		world.planes[0].mat.reflection = {0.1f, 0.1f, 0.1f};
-        world.planes[0].mat.mirror = false;
+		world.planes[0].mat.reflection = {0.4f, 0.4f, 0.4f};
+        world.planes[0].mat.mirror = true;
         world.geometries[world.geometryCount].id = world.planeCount;
         world.geometries[world.geometryCount].type = Geo_Plane;
         world.geometryCount += 1;
         world.planeCount += 1;
     }
     
-    world.dirLight.dir = {-0.5f, 0.5f, -0.5f};
-    world.dirLight.color = {0.4f, 0.4f, 0.4f};
+    world.dirLight.dir = {-0.5f, -0.5f, -0.5f};
+    world.dirLight.color = {0.1f, 0.1f, 0.1f};
     
     world.pointLightCount = 0;
     world.pointLights[world.pointLightCount].pos = {-2.0f, 1.0f, 4.0f};
-    world.pointLights[world.pointLightCount].color = {0.3f, 0.3f, 1.0f}; // BLUE
+    world.pointLights[world.pointLightCount].color = {0.3f, 0.3f, 0.3f}; // BLUE
     world.pointLightCount += 1;
 	
 	return world;
