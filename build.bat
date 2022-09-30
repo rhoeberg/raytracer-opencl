4eb656811d1cd26725edb31b3b9963cf31c97bce
rhoeberg <contact@rhoeberg.com>
2022-09-30 14:46:39 +0200

    changed to x64 build

--- build.bat
+++ build.bat
@@ -1,5 +1,4 @@
 SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community
-rem IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\Auxiliary\Build\vcvars32.bat" %1))
 IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\Auxiliary\Build\vcvars64.bat" %1))
 
 set includeFlags=-I ..\3rdparty\glfw\include -I ..\3rdparty\glew\include -I ..\3rdparty
@@ -7,9 +6,11 @@ set libraryFlags=glfw3dll.lib OpenGL32.lib glew32.lib OpenCL.lib /link /LIBPATH:
 
 IF NOT EXIST build (
    mkdir build
-   rem xcopy 3rdparty\glfw\lib\glfw3.dll build\
-   rem xcopy 3rdparty\glew\bin\glew32.dll build\
 )
+
+xcopy 3rdparty\glfw\lib\glfw3.dll build\ /Y
+xcopy 3rdparty\glew\bin\Release\x64\glew32.dll build\ /Y
+
 xcopy raytracer-kernel.cl build\ /Y
 xcopy fragmentshader.frag build\ /Y
 xcopy vertexshader.vs build\ /Y
